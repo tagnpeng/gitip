66db23de7d5d548c1f755a7e1346ae801d81bec0
simple <tangpengzy@163.com>
2023-08-21 13:13:31 +0800

    Initial Commit

---
+++ getip2/getip2App.swift
@@ -0,0 +1,17 @@
+//
+//  getip2App.swift
+//  getip2
+//
+//  Created by 汤鹏 on 2023/8/21.
+//
+
+import SwiftUI
+
+@main
+struct getip2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
